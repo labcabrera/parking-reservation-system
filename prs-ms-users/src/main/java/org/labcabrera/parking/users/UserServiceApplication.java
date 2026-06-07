79ff569bccc3771224f9bf6621f99dd9e1b551d8
labcabrera <lab.cabrera@gmail.com>
2026-06-07 16:37:23 +0200

    Review.

--- prs-ms-users/src/main/java/org/labcabrera/parking/users/UserServiceApplication.java
+++ prs-ms-users/src/main/java/org/labcabrera/parking/users/UserServiceApplication.java
@@ -1,12 +1,12 @@
-package org.labcabrera.parking.payment;
+package org.labcabrera.parking.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentServiceApplication {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentServiceApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
